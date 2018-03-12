741d53c627132cfd3803592af0247cd659a91f29
james tak <jtak93@gmail.com>
2018-03-11 21:48:58 -0700

    removes counter reducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
-import counterReducer from '../Counter/reducer'
 import githubReducer from '../Github/reducer'
 
 export default combineReducers({
   routing: routerReducer,
-  counter: counterReducer,
   github: githubReducer,
 })
